6f9508a259463c13fb886218f448ac16c3fe3762
sai-krishna26 <saikrishnavishwakarma@gmail.com>
2026-06-11 12:20:39 +0530

    updated project

--- online-transaction/src/main/java/com/xworkz/transDetail/platformDetails/GetPlatformDetails.java
+++ online-transaction/src/main/java/com/xworkz/transDetail/platformDetails/GetPlatformDetails.java
@@ -1,13 +1,47 @@
 package com.xworkz.transDetail.platformDetails;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
+
 public class GetPlatformDetails {
     public static void main(String[] args) {
         System.out.println("checking the GetPlatformDetails class existence");
         try {
-            Class.forName("com.mysql.cj.jdbc.Drive");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("GetPlatformDetails is found!!");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
+        }
+        Connection connection=null;
+        Statement statement=null;
+        try {
+           connection=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/payment_db","root","root");
+           statement=connection.createStatement();
+           int check=statement.executeUpdate("update patient_history set amount=5000.00 where patient_name='abhishek'");
+            System.out.println("result of update: "+check);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            if(Objects.nonNull(connection))
+            {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if(statement!=null)
+            {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
     }
 }
--- online-transaction/src/main/java/com/xworkz/transDetail/recieverDeatails/GetReceiverDetails.java
+++ online-transaction/src/main/java/com/xworkz/transDetail/recieverDeatails/GetReceiverDetails.java
@@ -1,16 +1,61 @@
 package com.xworkz.transDetail.recieverDeatails;
 
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+import java.util.Objects;
+
 public class GetReceiverDetails
 {
     public static void main(String[] args) {
         System.out.println("checking the GetReceiverDetails class existence");
         try{
-            Class.forName("com.xworkz.transDetail.platformDetails.GetPlatformDetails");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("GetReceiverDetails is found!!");
         }
         catch(ClassNotFoundException e)
         {
             throw new RuntimeException(e);
+        }
+
+        Connection connection=null;
+        Statement statement=null;
+        String query="select * from patient_history";
+        ResultSet resultSet=null;
+        try {
+            connection=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/payment_db","root","root");
+            statement=connection.createStatement();
+            resultSet=statement.executeQuery(query);
+
+            while (resultSet.next())
+            {
+                System.out.println(resultSet.getString("patient_name"));
+                System.out.println(resultSet.getDate("admition_date"));
+                System.out.println(resultSet.getFloat("amount"));
+                System.out.println(resultSet.getBoolean("payment_status"));
+                System.out.println(resultSet.getDate("dischagre_date"));
+                System.out.println("-------------------------------------------------------");
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            if(Objects.nonNull(connection))
+            {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (Objects.nonNull(statement))
+            {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
     }
 }
